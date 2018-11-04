ff1644cd789d06f8a0d6ef54d99b3863b66a0908
eber <eberjimenez@gmail.com>
2018-11-03 20:22:11 -0600

    index.php

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */
-
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">>
+	<title>Registro de Usuarios</title>
+	<meta content='width=device-width, initial-scale=1, maximum-scale=1,user-scalable=no' name='viewport'>
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrapcdn/3.2.0/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome.min.css"/>
+	<link rel="stylesheet" type="text/css" href="static/css/AdminLTE.css"/>
+</head>
+<body class="bg-black">
+	<div class="form-box" id="login-box">
+		<div class="header">Registro de Usuarios</div>
+		<form action="RegistrarUsuarios.php" method="post">
+			<div class="body bg-black">
+				<div class="form-group">
+					<input type="text" name="name" class="form-control" placeholder="Nombre Completo"/>
+				</div>
+				<div class="form-group">
+					<input type="email" name="email" class="form-control" placeholder="Email"/>
+				</div>
+			</div>
+			<div class="footer">
+				<button id="btnRegistrar" type="submit" class="btn bg-olive btn-block">Enviar</button>
+			</div>
+		</form>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>>
